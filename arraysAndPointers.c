71cec3de3c72e7454d6c7182dc679b5a3bcd10fa
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-08-26 06:25:14 -0400

    arrays and pointers

---
+++ arraysAndPointers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//pointers store the address of any variable of that particular data type 
+char c = 'A';
+char *pc = &c;//pc(pointer variable) stores the address of the character variable 'c'
+//c is a scalar value
+
+
+//
+
+
+int main()
+{
+
+
+    return 0;
+}
