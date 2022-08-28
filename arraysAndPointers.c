3984c948e88eecd33324c3fde5f4d964d9e4dda1
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-08-28 06:19:23 -0400

    arrays and pointers

--- arraysAndPointers.c
+++ arraysAndPointers.c
@@ -6,12 +6,30 @@ char *pc = &c;//pc(pointer variable) stores the address of the character variabl
 //c is a scalar value
 
 
-//
+//pointers to arrays
+
+char vowels[] = {'A', 'E', 'I', 'O', 'U'};
+char *pvowels = vowels;
+int i;
 
 
 int main()
 {
+    //print the addresses
+    for (i =0; i < 5; i++)
+    {
+        printf("&vowels[%d]: %p, pvowels + %d: %p, vowels + %d: %p\n", i, &vowels[i], i, pvowels + i, i, vowels + i );
+    }
 
+    //&vowels[i] - gives the memory loaction of the ith element of the array vowels
+    //pvowels took the address of vowels
+    //pvowels + i is a valid op
+
+    // Print the values
+    for (i = 0; i < 5; i++) 
+    {
+        printf("vowels[%d]: %c, *(pvowels + %d): %c, *(vowels + %d): %c\n", i, vowels[i], i, *(pvowels + i), i, *(vowels + i));
+    }
 
     return 0;
 }
