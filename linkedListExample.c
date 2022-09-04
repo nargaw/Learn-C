addcdf0e8d01eef162794411915aa8cd5e62528a
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-09-04 07:21:59 -0400

    linked list example

--- linkedListExample.c
+++ linkedListExample.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-
-int main(int argc, char const *argv[])
+struct sPerson
 {
-    /* code */
+    int age;
+};
+
+
+
+int main()
+{
+    //create a new Pointer to sPerson structure
+    struct sPerson *newPerson = NULL;
+
+    //allocate dynamic memory
+    //malloc returns a pointer to the beginning of the allocated memory
+    //point the newPerson pointer to the allocated returned pointer
+    newPerson = malloc(sizeof(struct sPerson));
+
+    //print the memory address of where the pointer is pointing
+    printf("new person at %p\n", newPerson);
     return 0;
 }
