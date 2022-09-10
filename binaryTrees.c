dee1c6b8346dc7691bb422d33560758bd4260230
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-09-10 06:44:04 -0400

    binary trees

---
+++ binaryTrees.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Binary Trees 
+ * -A binary tree is a type of data structure in which each node has at most two children 
+ * -common operations include insertion, deletion and traversal
+ * -leaf nodes or branch nodes
+ * -Balanced trees: the depth of left and right subtrees of every node differ by 1 or less
+ *  allows for predictable depth -> Height
+ * -measure of a node from root to leaf, root is 0 and subsequent nodes are (1, 2, ... n) = log2(n)
+ * 
+ * 
+ * Two types of searches:
+ * 1. Depth-first search (DFS): an algorithm for traversing or searching tree or graph data structures.
+ * -starts at root and explores as far as possible along each branch before back tracking
+ * -three types: 
+ * a. Pre-order visit, left, right
+ * b. in-order left, visit, right
+ * c. post-order left, right, visit
+ * 
+ * 2. Breadth-first search(BFS): an alogrithm for traversing or searching tree or graph structures, visit every node on a level before going to a lower level
+ */
+
+int main()
+{
+    return 0;
+}
