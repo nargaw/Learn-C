082ac4a99d225873bc6b0ee879821d28d01899e7
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-09-11 14:28:21 -0400

    binary tree ex 1

--- binaryTreeExample1.c
+++ binaryTreeExample1.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct BstNode
+{
+    int data;
+    struct BstNode* left;
+    struct BstNode* right;
+};
+//BstNode: binary search tree node
+//each BstNode has a left and right nodes to store a pointer to left or right child respectively
+
+
+struct BstNode* GetNewNode(int data)
+{
+    struct BstNode *newNode = NULL;
+    newNode = malloc(sizeof(struct BstNode));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+//data to be inserted as a node
+
 
 int main()
 {
+    struct BstNode* rootPtr; //store address of root node
+    rootPtr = NULL;
+    //rootPtr is not assigned yet
+
+    Insert(&rootPtr, 15);
+    Insert(&rootPtr, 25);
+    Insert(&rootPtr, 35);
+
+
     return 0;
 }
