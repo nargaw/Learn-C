c84d57add182b432fe605b68a8df361fb7ae29f8
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-09-12 16:59:05 -0400

    bt ex. 2

--- binaryTreeExample2.c
+++ binaryTreeExample2.c
@@ -22,6 +22,43 @@ treenode *createNode(int value)
     return result;
 }
 
+void printTabs(int numTabs)
+{
+    for(int i=0; i < numTabs; i++)
+    {
+        printf("\t");
+    }
+}
+
+void printTree(treenode* root)
+{
+    printTree_rec(root, 0);
+}
+
+void printTree_rec(treenode *root, int level)
+{
+    if(root == NULL)
+    {
+        printTabs(level);
+        printf("----<Empty>------\n");
+        return;
+    }
+
+    //preorder traversal
+    printTabs(level);
+    printf("value = %d\n", root->value);
+    printTabs(level);
+    printf("left\n");
+
+    printTree_rec(root->left, level + 1);
+    printTabs(level);
+    printf("right\n");
+
+    printTree_rec(root->right, level + 1);
+    printTabs(level);
+    printf("done\n");
+}
+
 
 int main()
 {
@@ -30,5 +67,17 @@ int main()
     treenode *n3 = createNode(12);
     treenode *n4 = createNode(13);
     treenode *n5 = createNode(14);
+
+    n1->left = n2;
+    n1->right = n3;
+    n3->left = n4;
+    n3->right = n5;
+
+    free(n1);
+    free(n2);
+    free(n3);
+    free(n4);
+    free(n5);
+
     return 0;
 }
